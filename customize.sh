946e17ce670e3048e9227072a8583cfcb62f16ad
linuo21 <linuo21@jd.com>
2026-03-01 00:55:54 +0800

    da

--- customize.sh
+++ customize.sh
@@ -1,6 +1,11 @@
 #!/system/bin/sh
 ui_print "- Installing App KeepAlive Module..."
 ui_print "- Creating config directory..."
+
 mkdir -p /data/adb/termux-keepalive
+chmod 0755 "$MODPATH/service.sh"
+chmod 0755 "$MODPATH/daemon.sh"
+chmod 0755 "$MODPATH/keepalive.sh"
+
 ui_print "- Done! Edit service.sh to configure your apps."
 ui_print "- Log file: /data/adb/termux-keepalive/keepalive.log"
---
+++ daemon.sh
@@ -0,0 +1,35 @@
+#!/system/bin/sh
+
+MODDIR=${0%/*}
+TARGET="$MODDIR/keepalive.sh"
+PIDFILE="/data/adb/termux-keepalive/daemon.pid"
+LOG="/data/adb/termux-keepalive/daemon.log"
+
+mkdir -p /data/adb/termux-keepalive
+
+log() {
+    echo "[daemon $(date '+%F %T')] $1" >> "$LOG"
+}
+
+start_worker() {
+    log "starting keepalive worker"
+    nohup sh "$TARGET" >> "$LOG" 2>&1 &
+    echo $! > "$PIDFILE"
+}
+
+is_alive() {
+    [ -f "$PIDFILE" ] || return 1
+    pid=$(cat "$PIDFILE")
+    kill -0 "$pid" 2>/dev/null
+}
+
+log "daemon started"
+
+while true; do
+    if ! is_alive; then
+        log "worker dead → restarting"
+        start_worker
+    fi
+
+    sleep 15
+done
